1d81f7ef2841664bc41e0394688f672882f1cdb5
bjaus <brandon.jaus@gmail.com>
2018-10-30 09:55:56 -0500

    removed extraneous styling/code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 import Colorizer from './components/Colorizer';
 
 class App extends Component {
   render() {
-
     return (
       <div className="App">
         <div className="App-header">
