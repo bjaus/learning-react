6de812a755b60606feb0b0f053fdb6f62d20ec62
bjaus <brandon.jaus@gmail.com>
2018-10-31 00:22:04 -0500

    touched on react-redux

---
+++ react-spa/src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Got Questions?</h2>
+        <p>
+          The easiest thing to do is visit 
+          my <a href='https://www.facebook.com/brandon.jaus'>Facebook</a> page.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ react-spa/src/Stuff.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stuff</h2>
+        <p>
+          Cras facilisis urna ornare ex volutpat, et
+          convallis erat elementum:
+        </p>
+        <ol>
+          <li>Nulla pulvinar diam</li>
+          <li>Facilisis bibendum</li>
+          <li>Vestibulum vulputate</li>
+          <li>Eget erat</li>
+          <li>Id porttitor</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
